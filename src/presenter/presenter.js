6f8bcbf075ffd8fac44cd13346f62d1a085dbd62
Полина Подвигина <podviginapolina@mail.ru>
2026-03-22 15:25:34 +0500

    feat: добавлен InfoView с информацией о путешествии

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,6 +1,7 @@
 import { render } from '../framework/render.js';
 import SortingView from '../view/sort-view.js';
 import FiltersView from '../view/filters-view.js';
+import InfoView from '../view/info-view.js';
 import EventPresenter from './event-presenter.js';
 
 export default class Presenter {
@@ -12,6 +13,7 @@ export default class Presenter {
 
   #filtersComponent = null;
   #sortingComponent = null;
+  #infoComponent = null;
   #eventPresenters = new Map();
 
   constructor({ filtersContainer, eventsContainer, eventsModel, offersModel, destinationsModel }) {
@@ -23,18 +25,79 @@ export default class Presenter {
   }
 
   init() {
+    this.#renderInfo();
     this.#renderFilters();
     this.#renderSorting();
     this.#renderEvents();
   }
 
+  #renderInfo() {
+    const events = this.#eventsModel.getAllFullEvents();
+
+    if (events.length === 0) {
+      return;
+    }
+
+    const destinations = events.map((event) => {
+      if (this.#destinationsModel) {
+        const destination = this.#destinationsModel.getById(event.destination);
+        return destination?.name || '';
+      }
+      return event.destination?.name || '';
+    }).filter(Boolean);
+
+    const uniqueDestinations = [...new Set(destinations)];
+    let title = '';
+    if (uniqueDestinations.length === 1) {
+      title = uniqueDestinations[0];
+    } else if (uniqueDestinations.length === 2) {
+      title = `${uniqueDestinations[0]} &mdash; ${uniqueDestinations[1]}`;
+    } else if (uniqueDestinations.length > 2) {
+      title = `${uniqueDestinations[0]} &mdash; ... &mdash; ${uniqueDestinations[uniqueDestinations.length - 1]}`;
+    }
+
+    const dates = events.map((event) => new Date(event.dateFrom));
+    const minDate = new Date(Math.min(...dates));
+    const maxDate = new Date(Math.max(...dates));
+
+    const formatDate = (date) => date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+
+    const dateString = `${formatDate(minDate)}&nbsp;&mdash;&nbsp;${formatDate(maxDate)}`;
+
+    const totalCost = events.reduce((sum, event) => {
+      let eventCost = event.basePrice;
+      if (event.offers && this.#offersModel) {
+        event.offers.forEach((offerId) => {
+          const offer = this.#offersModel.getById(offerId);
+          if (offer) {
+            eventCost += offer.price;
+          }
+        });
+      }
+      return sum + eventCost;
+    }, 0);
+
+    this.#infoComponent = new InfoView(title, dateString, totalCost);
+    const tripMain = document.querySelector('.trip-main');
+    if (tripMain) {
+      const tripControls = tripMain.querySelector('.trip-main__trip-controls');
+      if (tripControls) {
+        tripMain.insertBefore(this.#infoComponent.element, tripControls);
+      } else {
+        render(this.#infoComponent, tripMain);
+      }
+    }
+  }
+
   #renderFilters() {
-    this.#filtersComponent = new FiltersView();
-    render(this.#filtersComponent, this.#filtersContainer);
+    this.#filtersComponent = new FiltersView('everything');
+    if (this.#filtersContainer) {
+      render(this.#filtersComponent, this.#filtersContainer);
+    }
   }
 
   #renderSorting() {
-    this.#sortingComponent = new SortingView();
+    this.#sortingComponent = new SortingView('day');
     render(this.#sortingComponent, this.#eventsContainer);
   }
 
@@ -65,6 +128,9 @@ export default class Presenter {
 
   #handleEventChange(updatedEvent) {
     this.#eventsModel.updateEvent(updatedEvent);
+
+    this.#infoComponent?.removeElement();
+    this.#renderInfo();
 
     const presenter = this.#eventPresenters.get(updatedEvent.id);
     if (presenter) {
